bc985d211686e9098ff85e0a6df18b866f3854b5
tonefarhoud3433-ai <tonefarhoud3433@gmail.com>
2026-03-15 13:27:16 +0200

    second

--- src/Components/MasterLayout/MasterLayout.tsx
+++ src/Components/MasterLayout/MasterLayout.tsx
@@ -1,12 +1,13 @@
 import SideBar from '../SideBar/SideBar'
 import { Outlet } from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
+import styles from "./MasterLayout.module.css"
 
 export default function MasterLayout() {
   return (
     <>
       <div className="d-flex">
-          <div>
+          <div className={styles.sidebar}>
             <SideBar/>
           </div>
           <div className="w-100">
